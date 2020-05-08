3667fea5bbc4a316f1cd015b44401ca75d567bf3
Kiingy <7860411+Kiingy@users.noreply.github.com>
2020-05-08 16:39:07 +1000

    Update ErrorCollector.cs

--- src/MuteWowSounds/ClassFiles/ErrorCollector.cs
+++ src/MuteWowSounds/ClassFiles/ErrorCollector.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MuteWowSounds.ClassFiles {
     class ErrorCollector {
